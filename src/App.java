f7fcbf1e010e13d8bd0ea904605f9fa1421bbae5
Andre <andrermax111@gmail.com>
2022-07-24 12:47:41 -0300

    adição de exibição de dados

--- src/App.java
+++ src/App.java
@@ -15,11 +15,17 @@ public class App {
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
 
         // extrair somente os dados que serão utilizados (pôster, titulo e classificação)
-        var parser = new JsonParser();
+        var parser = new JsonParser(); 
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
         // exibir e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes)  {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
